cdec7ddfc9ca2f57fcc86b4d0e20de4ca01c0e8a
Jonathan Core <56272039+JCoreMS@users.noreply.github.com>
2023-01-26 14:42:48 -0500

    Add files via upload

--- Register-HostPool.ps1
+++ Register-HostPool.ps1
@@ -3,21 +3,30 @@
     Param(
         [parameter(Mandatory)]
         [string]
-        $HostPoolRegistrationToken
+        $HostPoolRegistrationToken,
+        [parameter(Mandatory)]
+        [string]
+        $XTenantRegister,
+        [parameter(Mandatory)]
+        [string]
+        $XTenantRegToken
     )
     
-    ##############################################################
-    #  Install the AVD Agent
-    ##############################################################
-    # Disabling this method for installing the AVD agent until AAD Join can completed successfully
-    $BootInstaller = 'AVD-Bootloader.msi'
-    Get-WebFile -FileName $BootInstaller -URL 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH'
-    Start-Process -FilePath 'msiexec.exe' -ArgumentList "/i $BootInstaller /quiet /qn /norestart /passive" -Wait -Passthru
-    Write-Log -Message 'Installed AVD Bootloader' -Type 'INFO'
-    Start-Sleep -Seconds 5
+##############################################################
+#  Install the AVD Agent
+##############################################################
+# Determine cross Tenant registration 
+If($XTenantRegister){$HostPoolRegistrationToken = $XTenantRegToken}
 
-    $AgentInstaller = 'AVD-Agent.msi'
-    Get-WebFile -FileName $AgentInstaller -URL 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv'
-    Start-Process -FilePath 'msiexec.exe' -ArgumentList "/i $AgentInstaller /quiet /qn /norestart /passive REGISTRATIONTOKEN=$HostPoolRegistrationToken" -Wait -PassThru
-    Write-Log -Message 'Installed AVD Agent' -Type 'INFO'
-    Start-Sleep -Seconds 5
+# Disabling this method for installing the AVD agent until AAD Join can completed successfully
+$BootInstaller = 'AVD-Bootloader.msi'
+Get-WebFile -FileName $BootInstaller -URL 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH'
+Start-Process -FilePath 'msiexec.exe' -ArgumentList "/i $BootInstaller /quiet /qn /norestart /passive" -Wait -Passthru
+Write-Log -Message 'Installed AVD Bootloader' -Type 'INFO'
+Start-Sleep -Seconds 5
+
+$AgentInstaller = 'AVD-Agent.msi'
+Get-WebFile -FileName $AgentInstaller -URL 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv'
+Start-Process -FilePath 'msiexec.exe' -ArgumentList "/i $AgentInstaller /quiet /qn /norestart /passive REGISTRATIONTOKEN=$HostPoolRegistrationToken" -Wait -PassThru
+Write-Log -Message 'Installed AVD Agent' -Type 'INFO'
+Start-Sleep -Seconds 5
